5d307a3ea67650eda28cc7f238c01a409fdcf9eb
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2018-08-13 18:20:53 -0700

    Added basic structure to project files

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let myLibrary = []
+
+function Book(title, author, pageCount, read) {
+	this.title = title;
+	this.author = author; 
+	this.pageCount = pageCount;
+	this.read = read;
+}
+
+function addBookToLibrary() {
+	
+}
